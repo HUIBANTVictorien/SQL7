3f263eef37f7fd605b197a1e80c764b2d7d0ebda
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-11-20 09:44:22 +0100

    ajout partie 7 sql

---
+++ sql7.sql
@@ -0,0 +1,17 @@
+Exercice 1
+
+Dans la table languages, supprimer toutes les lignes parlant de HTML.
+
+Commande en ligne de commande linux : DELETE FROM languages WHERE language='HTML';
+
+Exercice 2
+
+Dans la table frameworks, modifier toutes les lignes ayant le framework Symfony par Symfony2.
+
+Commande en ligne de commande linux : UPDATE frameworks SET frameworks='Symfony2' WHERE id=1 OR id=2;
+
+Exercice 3
+
+Dans la table languages, modifier la ligne du langage JavaScript version 5 par la version 5.1
+
+Commande en ligne de commande linux : UPDATE languages SET version=5.1 WHERE id=1;
